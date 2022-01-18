7ef1567ec2a55d7e99ca79b315b1be0298fd8086
Lahiru Jayawardhana <71606225+repo-son@users.noreply.github.com>
2022-01-18 17:09:53 +0530

    Implemented POST Operation via a Fake API

---
+++ TestLogin/TestLogin/Models/PostContent.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLogin.Models
+{
+    public class PostContent
+    {
+
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
